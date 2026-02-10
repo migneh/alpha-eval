89251c49fd00f8ad6227a092148d1ae3b6e33631
migneh <150557976+migneh@users.noreply.github.com>
2026-02-10 20:51:18 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers
+  ]
+});
+
+client.once("ready", () => {
+  console.log(`✅ Logged in as ${client.user.tag}`);
+});
+
+client.on("messageCreate", async message => {
+  if (message.author.bot || !message.guild) return;
+
+  require("./systems/pointS")(client, message);
+});
+
+client.login("PUT_YOUR_BOT_TOKEN_HERE");
